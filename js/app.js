64c8f155aa872bdecfce294f1ba3fb3f9c4065c1
Margaret Rozario <113563313+margaret-rozario@users.noreply.github.com>
2023-08-02 21:50:51 -0400

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
+function chnageContent(getValue){
+    let background = document.querySelector('.bgcolor');
+    let selectOption = getValue.value;
+    background.style.background = selectOption;
 
+    let selecttext = document.querySelector('.card');
+    let textValue = getValue.options[getValue.selectedIndex].text;
+    selecttext.innerHTML = textValue;
+
+}
